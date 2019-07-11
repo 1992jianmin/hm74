bf1e7d180fa6b13179936e691063755ad7279efc
1992jianmin <510780924@qq.com>
2019-07-11 21:31:57 +0800

    登录页OK

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
+import ELEMENT from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
-Vue.use(ElementUI)
+import axios from 'axios'
+Vue.prototype.$http = axios
+Vue.use(ELEMENT)
 
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,71 @@
 <template>
 <div class="login-container">
     <el-card class="login-box">
-        <img src="../../assets/images/logo_index.png" alt="">
+        <img src="../../assets/images/logo_index.png" alt="" >
+    <el-form ref='loginForm' :model="loginForm" :rules="loginRules" :status-icon="true">
+        <el-form-item  prop="mobile">
+        <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+        <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
+        <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+            <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+            <el-button style="width:100%" type="primary" @click="login()">登 录</el-button>
+             </el-form-item>
+    </el-form>
     </el-card>
 </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    const checkMobile = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不正确'))
+      }
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入正确的手机号码', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入正确的验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      //   对整体进行验证
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // 提交登录
+          this.$http
+            .post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或验证码不正确')
+            })
+        }
+      })
+    }
+  }
 }
 </script>
 
@@ -22,7 +79,7 @@ export default {
     background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
     .login-box{
         width: 400px;
-        height: 300px;
+        height: 330px;
         position: absolute;
         left: 50%;
         top: 50%;
